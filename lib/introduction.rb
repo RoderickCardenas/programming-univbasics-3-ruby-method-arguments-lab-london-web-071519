b58d8e720d41544ae7de1e1836adec3e79ff6192
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-06-26 13:31:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  "Hi, my name is #{name} and I am learning to program #{language}.\n"
+  puts "Hi, my name is #{name} and I am learning to program #{language}.\n"
 end
 
 def introduction_with_language_optional(name, language)
